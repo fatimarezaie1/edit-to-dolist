5008cb13fbb3ba32c8cb5c52133a55d8be1866ca
Fatima <fatimarezaie705@gmail.com>
2022-12-21 21:23:31 +0430

    edit to do list

--- behave.js
+++ behave.js
@@ -1,51 +1,38 @@
-let input= document.querySelector(".entered_list");
-let addbtn= document.querySelector(".add_list");
-let tasks= document.querySelector(".tasks");
+let input = document.querySelector('.entered_list');
+let addBtn = document.querySelector('.add_list');
+let tasks = document.querySelector('.takes');
 
-input.addEventListener('keyup',() => {
-    if(input.Value.trim() != 0){
-        addbtn.classList.add('active') ;
-    }
-else
-{
-    addbtn.classList.remove('active');
-}
-})
-addbtn.addEventListener('click', ()=>{
-    if (input.Value.trim() != 0){
-        let newItem = document.createElement('div');
-        newItem.classList.add(item);
-        newItem.innerHTML= "
-        <p> $ {input.value} </p>
-        <div class="item_btn">
-            <i class="fa-check"></i>
-            <i class="fa-trash"></i>
-        </div>
-        "
-        
-        tasks.appendChild(newItem)
-       input.Value='';
-    }
-       else {
-        alert('please enter a task')
-       }
 
+input.addEventListener('keyup',function() {
+    if(input.value.trim() != 0){
+        addBtn.classList.add('active');
     }
-)
-tasks.addEventListener('click', (e) =>{
-    if(e.target.classList.contains('fa-marks')){
-        e.target.parentelement.parentelement.remove();
-
-    }
-}
-)
-tasks.addEventListener ('click', e=>
-{
-    if(e.target.classList.contains(fa-pen-to-squre))
+    else 
     {
-        e.target.parentelement.parentelement.classList.toggle('completed');
+        addBtn.classList.remove('active');
     }
-}
+})
 
+addBtn.addEventListener('click', ()=>{
+    if(input.value.trim() != 0 ) {
+    let newItem = document.createElement('div');
+    newItem .classList.add('item');
+    newItem.innerHTML =  '<p >{input.value}</p><div class="item_btn"><i class="fa fa-trash" ></i><i class="fa fa-check"></i></div>';
+    tasks.appendChild(newItem);
+    input.value = " ";
+    }
+    else{
+        alert('please enter a task here!')
+    }
+})
+tasks.addEventListener('click',(e) =>{
+    if(e.target.classList.contains('fa-check')){
+        e.target.parentElement.parentElement.remove();
+    }
+})
+tasks.addEventListener('click',(e)=>{
+    if(e.target.classList.contains('fa-trash')){
+        e.target.parentElement.parentElement.classList.toggle('completed');
 
-)
+    }
+})
